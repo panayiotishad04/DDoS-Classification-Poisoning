af67e6defe349795625a34e053b47cf2b819091e
pc165 <pc165@pm.me>
2024-11-02 19:15:23 +0100

    explainer

---
+++ main.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras as k
+from tensorflow.keras import layers as l, models as m
+import shap
+
+from utils import COLUMNS, get_train_dataset, get_test_dataset
+
+np.random.seed(42)
+tf.random.set_seed(42)
+
+
+def get_trained_model():
+    model = make_classifier_model()
+
+    model.compile(optimizer='adam',
+                  loss="binary_crossentropy",
+                  metrics=['accuracy'])
+
+    model.summary()
+
+    x_train, y_train = get_train_dataset()
+    model.fit(x_train, y_train, epochs=50, batch_size=32, shuffle=True, verbose=0)
+
+    return model
+
+
+def make_classifier_model():
+    model = m.Sequential([
+        l.Input(shape=(len(COLUMNS),)),
+        l.Dense(64, activation='relu'),
+        l.Dense(32, activation='relu'),
+        l.Dense(1, activation='sigmoid')
+    ])
+
+    return model
+
+
+def make_generator_model():
+    model = m.Sequential([
+        l.Input(shape=(100,)),
+        l.Dense(32, activation='relu'),
+        l.Dense(64, activation='relu'),
+        l.Dense(len(COLUMNS), activation='tanh')
+    ])
+
+    return model
+
+
+if __name__ == "__main__":
+
+    def explain_classifier_model():
+        x, y = get_train_dataset()
+        x_test, y = get_test_dataset()
+
+        model = get_trained_model()
+
+        subset = x_test[:10]
+
+        explainer = shap.Explainer(model, x[:100])
+        shap_values = explainer(subset)
+        print(shap_values)
+
+        for shap_value in shap_values:
+            shap.plots.waterfall(shap_value)
+
+
+    def make_prediction():
+        x_test, y_test = get_test_dataset()
+        model = get_trained_model()
+        print(model.evaluate(x_test, y_test, return_dict=True))
+
+
+    explain_classifier_model()
---
+++ utils.py
@@ -0,0 +1,80 @@
+import pandas as pd
+
+#
+# def print(*args):
+#     pass
+
+
+TRAIN_COUNT = 500
+TEST_COUNT = 1000
+COLUMNS = [
+    'id.orig_port',
+    'id.resp_pport',
+    'orig_bytes_count',
+    'resp_bytes_count',
+    'missed_bytes_count',
+    'orig_pkts_count',
+    'orig_ip_bytes_count',
+    'resp_pkts_count',
+    'resp_bytes'
+]
+
+
+def create_small(name, n, random_state=42):
+    from zipfile import ZipFile
+
+    zipped_data = ZipFile("data.zip", mode="r")
+
+    good = pd.read_csv(zipped_data.open("Only_Benign.csv"))
+    bad = pd.read_csv(zipped_data.open("Only_DDOS.csv"))
+
+    assert good.shape[1] == bad.shape[1]
+
+    print(good.columns)
+    print(f"good count {good.shape[0]}, bad count {bad.shape[0]}")
+
+    small_good = good.sample(n=n, random_state=random_state)
+    small_bad = bad.sample(n=n, random_state=random_state)
+
+    small_good.replace("-", 0, inplace=True)
+    small_bad.replace("-", 0, inplace=True)
+
+    pd.DataFrame(small_bad).to_csv(f"bad_{name}.txt")
+    pd.DataFrame(small_good).to_csv(f"good_{name}.txt")
+
+
+def create_csv():
+    create_small("train", TRAIN_COUNT)
+    create_small("test", TEST_COUNT)
+
+
+def get_train_dataset():
+    good = pd.read_csv("good_train.txt")
+    bad = pd.read_csv("bad_train.txt")
+
+    df = pd.concat([good, bad])
+
+    df_train = df[COLUMNS]
+    y = df['Category'] == "Malicious"
+
+    assert sum(y) == TRAIN_COUNT
+
+    print("Good\n", good[COLUMNS].describe())
+    print("\n\n")
+    print("Bad\n", bad[COLUMNS].describe())
+
+    return df_train, y
+
+
+def get_test_dataset():
+    good = pd.read_csv("good_test.txt")
+    bad = pd.read_csv("bad_test.txt")
+
+    df = pd.concat([good, bad])
+
+    df_test = df[COLUMNS]
+    y = df['Category'] == "Malicious"
+
+    assert sum(y) == TEST_COUNT
+
+    return df_test, y
